f88e04f86dcfd377a4cb89e98e859eab42bd337f
Thiago Cunha de Moura Salles <tsalles@MacBook-Pro-de-Thiago.local>
2025-02-24 16:37:16 -0300

    Draft of deployment into codeengine as well as flag to enable/disable tls termination

--- run.sh
+++ run.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 
-CERTS_DIR="certs"
-KEY_FILE="$CERTS_DIR/key.pem"
-CSR_FILE="$CERTS_DIR/csr.pem"
-CERT_FILE="$CERTS_DIR/cert.pem"
+if [ "$TLS_TERMINATION" = "true" ]; then
+    echo "TLS termination is enabled."
 
-SUBJ="/C=BR/ST=Brazil/L=Brazil/O=The Organization/CN=textgen.api"
+    CERTS_DIR="certs"
+    KEY_FILE="$CERTS_DIR/key.pem"
+    CSR_FILE="$CERTS_DIR/csr.pem"
+    CERT_FILE="$CERTS_DIR/cert.pem"
 
-mkdir -p "$CERTS_DIR"
+    SUBJ="/C=BR/ST=Brazil/L=Brazil/O=The Organization/CN=textgen.api"
 
-if [ ! -f "$KEY_FILE" ] || [ ! -f "$CERT_FILE" ]; then
-    echo "SSL key or certificate not found. Generating self-signed certificates..."
-    openssl genpkey -algorithm RSA -out "$KEY_FILE"
-    openssl req -new -key "$KEY_FILE" -out "$CSR_FILE" -subj "$SUBJ"
-    openssl x509 -req -in "$CSR_FILE" -signkey "$KEY_FILE" -out "$CERT_FILE"
-    echo "Self-signed certificates generated in $CERTS_DIR."
+    mkdir -p "$CERTS_DIR"
+
+    if [ ! -f "$KEY_FILE" ] || [ ! -f "$CERT_FILE" ]; then
+        echo "SSL key or certificate not found. Generating self-signed certificates..."
+        openssl genpkey -algorithm RSA -out "$KEY_FILE"
+        openssl req -new -key "$KEY_FILE" -out "$CSR_FILE" -subj "$SUBJ"
+        openssl x509 -req -in "$CSR_FILE" -signkey "$KEY_FILE" -out "$CERT_FILE"
+        echo "Self-signed certificates generated in $CERTS_DIR."
+    else
+        echo "SSL key and certificate found in $CERTS_DIR."
+    fi
+
+    exec uvicorn main:app --host 0.0.0.0 --port 8000 --reload --ssl-keyfile="$KEY_FILE" --ssl-certfile="$CERT_FILE"
 else
-    echo "SSL key and certificate found in $CERTS_DIR."
+    echo "TLS termination is disabled. Running in HTTP mode."
+    exec uvicorn main:app --host 0.0.0.0 --port 8000 --reload
 fi
-
-exec uvicorn main:app --host 0.0.0.0 --port 8000 --reload --ssl-keyfile="$KEY_FILE" --ssl-certfile="$CERT_FILE"
